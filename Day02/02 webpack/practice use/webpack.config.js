862b54abb1165449beed13efde87d9bc1d17676a
guoxinming <guoxinming@zbj.com>
2021-09-21 14:21:46 +0800

    feat: webpack use

---
+++ Day02/02 webpack/practice use/webpack.config.js
@@ -0,0 +1,30 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  devServer: {
+    static: "./dist",
+  },
+  module: {
+    rules: [
+      {
+        // 匹配什么样子的文件
+        test: /\.css$/i,
+        // 使用loader ， 从后到前执行
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.json5$/i,
+        type: "json",
+        parser: {
+          parse: json5.parse,
+        },
+      },
+    ],
+  },
+};
